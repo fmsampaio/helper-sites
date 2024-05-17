89f20f5a4bfe8b8dfae75b5fb156cbaefa8bab8d
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2024-05-17 15:21:32 -0300

    Basic version of exp-logger-view app

---
+++ exp-logger-view/fetch-api.js
@@ -0,0 +1,44 @@
+const logEntriesTable = document.getElementById('log_entries_table')
+const BASE_API_URL = 'https://exp-logger-api-5bed46122227.herokuapp.com'
+
+fetchData()
+
+function fetchData() {
+    fetch(`${BASE_API_URL}/log_entries/`, {
+        method : 'GET',
+        headers : {
+            'Content-Type' : 'application/json'
+        }
+    })
+    .then( (resp) => resp.json() )
+    .then( (data) => {
+        data.forEach( logEntry => {
+            createLogEntryRow(logEntry)
+        });
+    })
+    
+}
+
+function createLogEntryRow(logEntry) {
+    const newRow = logEntriesTable.insertRow(-1);
+
+    const tdTimestamp = document.createElement('td')
+    tdTimestamp.innerHTML = logEntry.time_created
+    newRow.appendChild(tdTimestamp)
+
+    const tdProject = document.createElement('td')
+    tdProject.innerHTML = logEntry.project.title
+    newRow.appendChild(tdProject)
+
+    const tdExperiment = document.createElement('td')
+    tdExperiment.innerHTML = logEntry.experiment_name
+    newRow.appendChild(tdExperiment)
+
+    const tdLogMessage = document.createElement('td')
+    tdLogMessage.innerHTML = logEntry.log_message
+    newRow.appendChild(tdLogMessage)
+
+       
+}
+
+
